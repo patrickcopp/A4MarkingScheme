d578d19d4daa9cce1ea1c4df366e195d6dd675a4
socs <socs@socsvm.local>
2020-10-21 11:23:50 -0400

    Committing changes for updates tree.h

--- grade_comparNode.c
+++ grade_comparNode.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include "tree.h"
 
+int compareStrings(const void *s1, const void *s2)
+{
+	return strcmp((const char *)s1,(const char *)s2);
+}
+
 int main()
 {
 	struct Performance *perf= newPerformance();
@@ -10,7 +15,7 @@ int main()
 	char data[10];
 	strcpy(data,"CORRECT\0");
 	attachNode(perf,node,data,8);
-	if( perf->mallocs == 1 && perf->writes == 1 && perf->reads == 0 && perf->frees == 0 && (*node)->lt == NULL && (*node)->gte == NULL)
+	if( comparNode(perf,node,compareStrings,data)==0 && perf->mallocs == 1 && perf->writes == 1 && perf->reads == 1 && perf->frees == 0 && (*node)->lt == NULL && (*node)->gte == NULL)
 		printf("%s\r\n",(char *)((*node)->data));
 	free((*node)->data);
 	free(*node);
